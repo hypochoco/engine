01d00d82c83ca1f2a2603a5717bfff384d008cc7
Daniel Cho <daniel_s_cho@brown.edu>
2025-05-13 02:10:19 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "hello world" << std::endl;
+
+    // goal - zelda (skyward sword) like video game
+        // interface? Not sure...
+        // graphics? OpenGL
+        // physics? 
+
+    // what else might be interesting though... 
+    // interesting physics and shit... 
+
+    return 0;
+
+}
